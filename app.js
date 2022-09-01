4e523ff949bdaff1be6a7096d2986ebf6a7598f3
bita5000 <das2ome@gmail.com>
2022-09-01 20:25:53 +0900

    선 그리기

--- app.js
+++ app.js
@@ -4,25 +4,36 @@ const ctx = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 400;
 
-// // shortcut Function
-// // moveTo - 브러쉬의 좌표를 움직여줌
-// // lineTo - 라인을 그려줌
-// // fillRect = fill + Rect(=moveTo + lineTo)
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+    "#18dcff",
+    "#7d5fff",
+];
 
-ctx.fillRect(150, 150, 4, 50);
-ctx.fillRect(250, 150, 4, 50);
-ctx.fillRect(170, 150, 65, 80);
-// 원 그리는 방법
-// arc를 사용하여 원을 그려넣을 수 있다.
-// X축, Y축, 반지름 길이, 원 시작 위치, 시작 위치에서부터 원을 어디까지 그릴지
-ctx.arc(200, 120, 25, 0, 2 * Math.PI);
-ctx.fill();
+ctx.lineWidth = 2;
 
-// 새로운 경로를 만들어 줌
-// 지금의 경우 새로운 색상으로 변경해 주시 위해 beginPath를 사용하여 초기화(?)해주고
-// 새로운 색상을 넣어 원을 그려줌
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(190, 120, 5, Math.PI, 2 * Math.PI);
-ctx.arc(210, 120, 5, Math.PI, 2 * Math.PI);
-ctx.fill();
+function onClick(e) {
+    // 시작하기 전 캔버스에 있는 모든 선의 색상이 바뀌게 하지 않기 위해 beginPath를 사용하여 각각의 선의 색상을 정해줌
+    ctx.beginPath();
+
+    // 선을 그리기 전 선을 시작할 위치를 선정
+    ctx.moveTo(0, 0);
+
+    //색상 변경
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+
+    // 선을 끝마칠 위치를 선정
+    ctx.lineTo(e.offsetX, e.offsetY);
+
+    // 얘가 결정적으로 시작 위치와 끝 위치를 읽고 눈에 보이게 해 줌
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
